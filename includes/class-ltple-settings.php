52c4f6b1d4ec9a6c2b1ffc6c1680d3d0b916eb5f
Rafasashi\Raphael <raphael.dartigues@gmail.com>
2017-08-07 17:01:05 +0200

    first commit

---
+++ includes/class-ltple-settings.php
@@ -0,0 +1,125 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class LTPLE_Directory_Settings {
+
+	/**
+	 * The single instance of LTPLE_Directory_Settings.
+	 * @var 	object
+	 * @access  private
+	 * @since 	1.0.0
+	 */
+	private static $_instance = null;
+
+	/**
+	 * The main plugin object.
+	 * @var 	object
+	 * @access  public
+	 * @since 	1.0.0
+	 */
+	public $parent = null;
+
+	/**
+	 * Prefix for plugin settings.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $base = '';
+
+	/**
+	 * Available settings for plugin.
+	 * @var     array
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $settings = array();
+
+	public function __construct ( $parent ) {
+		
+		$this->parent = $parent;
+		
+		$this->plugin 		 	= new stdClass();
+		$this->plugin->slug  	= 'live-template-editor-directory';
+		
+		add_action('ltple_plugin_settings', array($this, 'plugin_info' ) );
+		
+		add_action('ltple_plugin_settings', array($this, 'settings_fields' ) );
+		
+		add_action( 'ltple_admin_menu' , array( $this, 'add_menu_items' ) );	
+	}
+	
+	public function plugin_info(){
+		
+		$this->parent->settings->addons['directory-plugin'] = array(
+			
+			'title' 		=> 'Directory Plugin',
+			'addon_link' 	=> 'https://github.com/rafasashi/live-template-editor-directory',
+			'addon_name' 	=> 'live-template-editor-directory',
+			'source_url' 	=> 'https://github.com/rafasashi/live-template-editor-directory/archive/master.zip',
+			'description'	=> 'This is a directory plugin for live template editor.',
+			'author' 		=> 'Rafasashi',
+			'author_link' 	=> 'https://profiles.wordpress.org/rafasashi/',
+		);		
+	}
+
+	/**
+	 * Build settings fields
+	 * @return array Fields to be displayed on settings page
+	 */
+	public function settings_fields () {
+		
+		$settings = [];
+
+		$settings['test'] = array(
+			'title'					=> __( 'Test', $this->plugin->slug ),
+			'description'			=> '',
+			'fields'				=> array(
+				
+				array(
+					'id' 			=> 'directory_url',
+					'label'			=> __( 'Directory Url' , $this->plugin->slug ),
+					'description'	=> '',
+					'type'			=> 'text',
+					'default'		=> '',
+					'placeholder'	=> __( 'http://', $this->plugin->slug )
+				),				
+			)
+		);
+		
+		if( !empty($settings) ){
+		
+			foreach( $settings as $slug => $data ){
+				
+				if( isset($this->parent->settings->settings[$slug]['fields']) && !empty($data['fields']) ){
+					
+					$fields = $this->parent->settings->settings[$slug]['fields'];
+					
+					$this->parent->settings->settings[$slug]['fields'] = array_merge($fields,$data['fields']);
+				}
+				else{
+					
+					$this->parent->settings->settings[$slug] = $data;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Add settings page to admin menu
+	 * @return void
+	 */
+	public function add_menu_items () {
+		
+		//add menu in wordpress dashboard
+
+		add_submenu_page(
+			'live-template-editor-client',
+			__( 'Directory test', $this->plugin->slug ),
+			__( 'Directory test', $this->plugin->slug ),
+			'edit_pages',
+			'edit.php?post_type=post'
+		);
+	}
+}
---
+++ includes/class-ltple.php
@@ -0,0 +1,264 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class LTPLE_Directory {
+
+	/**
+	 * The single instance of LTPLE_Directory.
+	 * @var 	object
+	 * @access  private
+	 * @since 	1.0.0
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Constructor function.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function __construct ( $file='', $parent, $version = '1.0.0' ) {
+
+		$this->parent = $parent;
+	
+		$this->_version = $version;
+		$this->_token	= md5($file);
+		
+		$this->message = '';
+		
+		// Load plugin environment variables
+		$this->file 		= $file;
+		$this->dir 			= dirname( $this->file );
+		$this->views   		= trailingslashit( $this->dir ) . 'views';
+		$this->vendor  		= WP_CONTENT_DIR . '/vendor';
+		$this->assets_dir 	= trailingslashit( $this->dir ) . 'assets';
+		$this->assets_url 	= esc_url( trailingslashit( plugins_url( '/assets/', $this->file ) ) );
+		
+		//$this->script_suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+		$this->script_suffix = '';
+
+		register_activation_hook( $this->file, array( $this, 'install' ) );
+		
+		// Load frontend JS & CSS
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ), 10 );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 10 );
+
+		// Load admin JS & CSS
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ), 10, 1 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_styles' ), 10, 1 );
+		
+		$this->settings = new LTPLE_Directory_Settings( $this->parent );
+		
+		$this->admin = new LTPLE_Directory_Admin_API( $this );
+
+		if ( !is_admin() ) {
+
+			// Load API for generic admin functions
+			
+			add_action( 'wp_head', array( $this, 'directory_header') );
+			add_action( 'wp_footer', array( $this, 'directory_footer') );
+		}
+		
+		// Handle localisation
+		
+		$this->load_plugin_textdomain();
+		
+		add_action( 'init', array( $this, 'load_localisation' ), 0 );
+		
+		//init profiler 
+		
+		add_action( 'init', array( $this, 'directory_init' ));	
+
+		// Custom editor template
+		
+		add_filter( 'template_include', array( $this, 'directory_template'), 1 );	
+		
+	} // End __construct ()
+	
+	public function directory_template( $template_path ){
+		
+		
+		return $template_path;
+	}
+	
+	public function directory_init(){	
+		
+		
+	}
+	
+	public function directory_header(){
+		
+		//echo '<link rel="stylesheet" href="https://raw.githubusercontent.com/dbtek/bootstrap-vertical-tabs/master/bootstrap.vertical-tabs.css">';	
+	}
+	
+	public function directory_footer(){
+		
+		
+	}	
+	
+	/**
+	 * Wrapper function to register a new post type
+	 * @param  string $post_type   Post type name
+	 * @param  string $plural      Post type item plural name
+	 * @param  string $single      Post type item single name
+	 * @param  string $description Description of post type
+	 * @return object              Post type class object
+	 */
+	public function register_post_type ( $post_type = '', $plural = '', $single = '', $description = '', $options = array() ) {
+
+		if ( ! $post_type || ! $plural || ! $single ) return;
+
+		$post_type = new LTPLE_Client_Post_Type( $post_type, $plural, $single, $description, $options );
+
+		return $post_type;
+	}
+
+	/**
+	 * Wrapper function to register a new taxonomy
+	 * @param  string $taxonomy   Taxonomy name
+	 * @param  string $plural     Taxonomy single name
+	 * @param  string $single     Taxonomy plural name
+	 * @param  array  $post_types Post types to which this taxonomy applies
+	 * @return object             Taxonomy class object
+	 */
+	public function register_taxonomy ( $taxonomy = '', $plural = '', $single = '', $post_types = array(), $taxonomy_args = array() ) {
+
+		if ( ! $taxonomy || ! $plural || ! $single ) return;
+
+		$taxonomy = new LTPLE_Client_Taxonomy( $taxonomy, $plural, $single, $post_types, $taxonomy_args );
+
+		return $taxonomy;
+	}
+
+	/**
+	 * Load frontend CSS.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return void
+	 */
+	public function enqueue_styles () {
+		
+		wp_register_style( $this->_token . '-frontend', esc_url( $this->assets_url ) . 'css/frontend.css', array(), $this->_version );
+		wp_enqueue_style( $this->_token . '-frontend' );
+	} // End enqueue_styles ()
+
+	/**
+	 * Load frontend Javascript.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function enqueue_scripts () {
+		
+		wp_register_script( $this->_token . '-frontend', esc_url( $this->assets_url ) . 'js/frontend' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
+		wp_enqueue_script( $this->_token . '-frontend' );
+	} // End enqueue_scripts ()
+
+	/**
+	 * Load admin CSS.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function admin_enqueue_styles ( $hook = '' ) {
+		
+		wp_register_style( $this->_token . '-admin', esc_url( $this->assets_url ) . 'css/admin.css', array(), $this->_version );
+		wp_enqueue_style( $this->_token . '-admin' );
+	} // End admin_enqueue_styles ()
+
+	/**
+	 * Load admin Javascript.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function admin_enqueue_scripts ( $hook = '' ) {
+		
+		wp_register_script( $this->_token . '-admin', esc_url( $this->assets_url ) . 'js/admin' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
+		wp_enqueue_script( $this->_token . '-admin' );
+	} // End admin_enqueue_scripts ()
+
+	/**
+	 * Load plugin localisation
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function load_localisation () {
+		
+		load_plugin_textdomain( $this->settings->plugin->slug, false, dirname( plugin_basename( $this->file ) ) . '/lang/' );
+	} // End load_localisation ()
+
+	/**
+	 * Load plugin textdomain
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function load_plugin_textdomain () {
+		
+	    $domain = $this->settings->plugin->slug;
+
+	    $locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+
+	    load_textdomain( $domain, WP_LANG_DIR . '/' . $domain . '/' . $domain . '-' . $locale . '.mo' );
+	    load_plugin_textdomain( $domain, false, dirname( plugin_basename( $this->file ) ) . '/lang/' );
+	} // End load_plugin_textdomain ()
+
+	/**
+	 * Main LTPLE_Directory Instance
+	 *
+	 * Ensures only one instance of LTPLE_Directory is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 * @static
+	 * @see LTPLE_Directory()
+	 * @return Main LTPLE_Directory instance
+	 */
+	public static function instance ( $file = '', $version = '1.0.0' ) {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self( $file, $version );
+		}
+		return self::$_instance;
+	} // End instance ()
+
+	/**
+	 * Cloning is forbidden.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __clone () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), $this->_version );
+	} // End __clone ()
+
+	/**
+	 * Unserializing instances of this class is forbidden.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __wakeup () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), $this->_version );
+	} // End __wakeup ()
+
+	/**
+	 * Installation. Runs on activation.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function install () {
+		$this->_log_version_number();
+	} // End install ()
+
+	/**
+	 * Log the plugin version number.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	private function _log_version_number () {
+		update_option( $this->_token . '_version', $this->_version );
+	} // End _log_version_number ()
+
+}
